906143b58bc62196fac7fafd1328dae21231f94b
Francesco Lafratta <fl.novacta@outlook.com>
2024-12-07 18:03:59 +0100

    Refactored grid moving functions and simplified main script.

--- 2024/day-4/solution.py
+++ 2024/day-4/solution.py
@@ -19,52 +19,76 @@ with open("input.txt", "r") as file:
         matrix.append(line_list)
 
 
-def go_up(row, col, grid):
-    if row == 0:
-        return None
-    return grid[row - 1][col]
+def go_up(row, col, grid, qty):
+    check_result = can_search_up(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row - i][col])
+    return result
 
 
-def go_down(row, col, grid):
-    if row == len(grid) - 1:
-        return None
-    return grid[row + 1][col]
+def go_down(row, col, grid, qty):
+    check_result = can_search_down(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row + i][col])
+    return result
 
 
-def go_right(row, col, grid):
-    if col == len(grid) - 1:
-        return None
-    return grid[row][col + 1]
+def go_right(row, col, grid, qty):
+    check_result = can_search_right(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row][col + i])
+    return result
 
 
-def go_left(row, col, grid):
-    if col == 0:
-        return None
-    return grid[row][col - 1]
+def go_left(row, col, grid, qty):
+    check_result = can_search_left(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row][col - i])
+    return result
 
 
-def go_up_right(row, col, grid):
-    if row == 0 or col == len(grid) - 1:
-        return None
-    return grid[row - 1][col + 1]
+def go_up_right(row, col, grid, qty):
+    check_result = can_search_up_right(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row - i][col + i])
+    return result
 
 
-def go_up_left(row, col, grid):
-    if row == 0 or col == 0:
-        return None
-    return grid[row - 1][col - 1]
+def go_up_left(row, col, grid, qty):
+    check_result = can_search_up_left(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row - i][col - i])
+    return result
 
 
-def go_down_right(row, col, grid):
-    if row == len(grid) - 1 or col == len(grid) - 1:
-        return None
-    return grid[row + 1][col + 1]
+def go_down_right(row, col, grid, qty):
+    check_result = can_search_down_right(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row + i][col + i])
+    return result
 
 
-def go_down_left(row, col, grid):
-    if col == 0 or row == len(grid) - 1:
-        return None
-    return grid[row + 1][col - 1]
+def go_down_left(row, col, grid, qty):
+    check_result = can_search_down_left(row, col, grid, qty)
+    result = []
+    if check_result:
+        for i in range(1, qty + 1):
+            result.append(grid[row + i][col - i])
+    return result
 
 
 def can_search_up(row, col, grid, qty):
@@ -115,7 +139,7 @@ def can_search_down_left(row, col, grid, qty):
     return True
 
 
-char_search_dict = {0: "M", 1: "A", 2: "S"}
+valid_target = ["M", "A", "S"]
 total_words = 0
 total_xmas_words = 0
 valid_permutations = [
@@ -128,95 +152,46 @@ valid_permutations = [
 for rindex, row in enumerate(matrix):
     for cindex, elem in enumerate(row):
         if elem == "X":
-            up = can_search_up(rindex, cindex, matrix, 3)
-            down = can_search_down(rindex, cindex, matrix, 3)
-            right = can_search_right(rindex, cindex, matrix, 3)
-            left = can_search_left(rindex, cindex, matrix, 3)
-            upright = can_search_up_right(rindex, cindex, matrix, 3)
-            upleft = can_search_up_left(rindex, cindex, matrix, 3)
-            downright = can_search_down_right(rindex, cindex, matrix, 3)
-            downleft = can_search_down_left(rindex, cindex, matrix, 3)
+            up = go_up(rindex, cindex, matrix, 3)
+            down = go_down(rindex, cindex, matrix, 3)
+            right = go_right(rindex, cindex, matrix, 3)
+            left = go_left(rindex, cindex, matrix, 3)
+            upright = go_up_right(rindex, cindex, matrix, 3)
+            upleft = go_up_left(rindex, cindex, matrix, 3)
+            downright = go_down_right(rindex, cindex, matrix, 3)
+            downleft = go_down_left(rindex, cindex, matrix, 3)
 
-            if up:
-                for i in range(3):
-                    result = go_up(rindex - i, cindex, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if up == valid_target:
+                total_words += 1
 
-            if down:
-                for i in range(3):
-                    result = go_down(rindex + i, cindex, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if down == valid_target:
+                total_words += 1
 
-            if right:
-                for i in range(3):
-                    result = go_right(rindex, cindex + i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if right == valid_target:
+                total_words += 1
 
-            if left:
-                for i in range(3):
-                    result = go_left(rindex, cindex - i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if left == valid_target:
+                total_words += 1
 
-            if upright:
-                for i in range(3):
-                    result = go_up_right(rindex - i, cindex + i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if upright == valid_target:
+                total_words += 1
 
-            if upleft:
-                for i in range(3):
-                    result = go_up_left(rindex - i, cindex - i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if upleft == valid_target:
+                total_words += 1
 
-            if downright:
-                for i in range(3):
-                    result = go_down_right(rindex + i, cindex + i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if downright == valid_target:
+                total_words += 1
 
-            if downleft:
-                for i in range(3):
-                    result = go_down_left(rindex + i, cindex - i, matrix)
-                    if result != char_search_dict[i]:
-                        break
-                else:
-                    total_words += 1
+            if downleft == valid_target:
+                total_words += 1
         elif elem == "A":
-            upright = can_search_up_right(rindex, cindex, matrix, 1)
-            upleft = can_search_up_left(rindex, cindex, matrix, 1)
-            downright = can_search_down_right(rindex, cindex, matrix, 1)
-            downleft = can_search_down_left(rindex, cindex, matrix, 1)
+            upright = go_up_right(rindex, cindex, matrix, 1)
+            upleft = go_up_left(rindex, cindex, matrix, 1)
+            downright = go_down_right(rindex, cindex, matrix, 1)
+            downleft = go_down_left(rindex, cindex, matrix, 1)
 
             if upright and upleft and downright and downleft:
-                result_upright = go_up_right(rindex, cindex, matrix)
-                result_upleft = go_up_left(rindex, cindex, matrix)
-                result_downright = go_down_right(rindex, cindex, matrix)
-                result_downleft = go_down_left(rindex, cindex, matrix)
-                result_list = [
-                    result_upright,
-                    result_upleft,
-                    result_downright,
-                    result_downleft,
-                ]
+                result_list = upright + upleft + downright + downleft
                 if result_list in valid_permutations:
                     total_xmas_words += 1
 
