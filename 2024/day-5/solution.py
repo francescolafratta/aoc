eb5d0e9f8eb55520b250623fcfe8d0822b758a21
Francesco Lafratta <fl.novacta@outlook.com>
2024-12-09 18:49:02 +0100

    Solve day 5 part 2.

--- 2024/day-5/solution.py
+++ 2024/day-5/solution.py
@@ -9,9 +9,21 @@ if not (os.path.exists("input.txt")):
     input.save_input(year=2024, day=5)
 
 precedence = {}
-correct_reports = 0
 sum_middle_numbers = 0
-rules = {}
+sum_sorted = 0
+
+def process_uncorrect(report):
+    result = report.copy()
+    myset = set(report)
+    for elem in report:
+        myset.remove(elem)
+        preceds = set(precedence[elem])
+        inter = myset.intersection(preceds)
+        length = len(inter)
+        result[length] = elem
+        myset.add(elem)
+    return result
+
 
 with open("input.txt", "r") as file:
     page_ordering = True
@@ -28,16 +40,17 @@ with open("input.txt", "r") as file:
         else:
             stripped_line = line.rstrip().split(",")
             length = len(stripped_line)
+            middle_index = int(length/2)
             correct_report = True
             for i in range(length - 1):
                 for j in range(i + 1, length):
                     if stripped_line[j] in precedence[stripped_line[i]]:
                         correct_report = False
             if correct_report:
-                correct_reports += 1
-                sum_middle_numbers += int(stripped_line[int(length/2)])
+                sum_middle_numbers += int(stripped_line[middle_index])
+            else:
+                result = process_uncorrect(stripped_line)
+                sum_sorted += int(result[middle_index])
                     
-
-print(stripped_line)
-print(correct_reports)
 print(sum_middle_numbers)
+print(sum_sorted)
